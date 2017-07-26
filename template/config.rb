f5f1e62e8954edadaff23838fe8242f4681d701a
Boris Paillard <boris.paillard@gmail.com>
2017-07-26 21:29:51 +0200

    home and fix helpers removing activate sprockets

--- template/config.rb
+++ template/config.rb
@@ -2,8 +2,6 @@ activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
-activate :sprockets
-
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
