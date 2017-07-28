d9e9e2cf55930b4722e72bd3d5249ff89260bfc0
Boris Paillard <boris.paillard@gmail.com>
2017-07-28 11:34:38 +0200

    adding about page an style

---
+++ about-design/Gemfile
@@ -0,0 +1,13 @@
+source 'https://rubygems.org'
+
+gem 'middleman', '~> 4.2'
+gem 'middleman-autoprefixer', '~> 2.7'
+gem 'middleman-sprockets'
+gem 'middleman-deploy', git: 'https://github.com/lewagon/middleman-deploy.git'
+gem 'pry-byebug', require: true
+
+# Assets
+gem 'font-awesome-middleman'
+gem 'bootstrap-sass'
+gem 'jquery-middleman'
+
---
+++ about-design/config.rb
@@ -0,0 +1,22 @@
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
+end
+
+activate :sprockets
+
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :asset_hash
+  activate :relative_assets
+  set :relative_links, true
+end
+
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.deploy_method = :git
+end
